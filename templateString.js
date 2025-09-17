aea9c5adf0c808a58e46c1733e164b194959da10
zobeir-rigi <rigi.zobir@yahoo.com>
2025-09-17 17:59:04 +0100

    practising back tik

---
+++ templateString.js
@@ -0,0 +1,6 @@
+//  string Backtick single quote and doble quote
+
+let number = false
+const expression = `john's money is {10+20}$ and my money is ${number?10:`nothing`}`
+
+console.log(expression)
