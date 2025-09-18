749155310b26b2aa8a8043e3c5c6483ff0c84abe
zobeir-rigi <rigi.zobir@yahoo.com>
2025-09-18 01:09:06 +0100

    Js and java typeof

--- type.js
+++ type.js
@@ -11,3 +11,10 @@ console.log(typeof(function(){}))
 
 console.log(typeof(true))
 console.log(typeof(false))
+
+// type are dynamic in javascript vs static in other languages like java, c, c++, c# etc
+// we can change the type of variable at any time
+let a = 10
+console.log(typeof(a))
+a = "hello" 
+// but in java, c, c++ etc we cannot change the type of variable once defined
