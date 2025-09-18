b3f6d00fb757fc42bc1c0a29516c8040ac65e787
zobeir-rigi <rigi.zobir@yahoo.com>
2025-09-18 01:07:24 +0100

    typeof in js

---
+++ type.js
@@ -0,0 +1,13 @@
+console.log(typeof(null))
+console.log(typeof(undefined))
+
+console.log(typeof(NaN))
+
+console.log(typeof([]))
+
+console.log(typeof({}))
+
+console.log(typeof(function(){}))
+
+console.log(typeof(true))
+console.log(typeof(false))
