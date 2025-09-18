121d278921d12969b967ce4fcc651f574549e36d
zobeir-rigi <rigi.zobir@yahoo.com>
2025-09-18 01:25:11 +0100

    nan ans isNaN()

--- type.js
+++ type.js
@@ -18,3 +18,7 @@ let a = 10
 console.log(typeof(a))
 a = "hello" 
 // but in java, c, c++ etc we cannot change the type of variable once defined
+
+// nan type mean not a number and it is of type number, and also it has a function isNaN() to check if a value is NaN or not
+console.log(isNaN(NaN)) // true
+console.log(isNaN(10)) // false
