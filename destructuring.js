adcc752828130e67e53eaacc6d67cebddc02de39
zobeir-rigi <rigi.zobir@yahoo.com>
2025-09-25 03:11:09 +0100

    destructuring

--- destructuring.js
+++ destructuring.js
@@ -1,4 +1,13 @@
 const obj = {name: "Zobeir", age :"33", city:"London" }
-const {name:first}=obj
-const {age:second}=obj
-console.log(first,second)
+// const {name:first}=obj
+// const {age:second}=obj
+
+const{age, city}=obj
+console.log(city)
+
+const arr = [1,2,3,4,5]
+// const first = arr[0]
+// const second = arr[1]
+
+const[first, second, ...other]=arr
+console.log(other)
